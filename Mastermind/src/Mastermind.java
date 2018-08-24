eeb3eb196e88632e89f1c8938fda918cef2e93f3
bbated <38528348+bbated@users.noreply.github.com>
2018-08-24 13:44:33 +0200

    mastermind start

---
+++ Mastermind/src/Mastermind.java
@@ -0,0 +1,7 @@
+
+public class Mastermind {
+	public static void main(String[] args) {
+		System.out.println("Geben Sie einen Versuchscode mit vier Buchstaben aus der Menge {r,g,b,y,s,w} ein:"
+				+ "\nSpiel beendet. Geheimcode war xxxx. Anzahl Versuche : 5");
+	}
+}
