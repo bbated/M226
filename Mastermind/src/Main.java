c1740fa65a110a6b6c5040306cea6526c712e384
bbated <38528348+bbated@users.noreply.github.com>
2018-09-11 14:44:10 +0200

    warnings entfehrnt

--- Mastermind/src/Main.java
+++ Mastermind/src/Main.java
@@ -1,14 +1,11 @@
-import java.util.Scanner;
-
-public class main {
+public class Main {
 	
 	private static Mastermind mastermind = new Mastermind();
 	
 	public static void main(String[] args) {
 			
 		mastermind.start();
-		
-		
+				
 	}
 
 }
