da29e34ad9cb64fc526a0195cda5971334ca25b1
bbated <38528348+bbated@users.noreply.github.com>
2018-12-14 13:19:48 +0100

    asdf

--- Minesweeper/src/Field.java
+++ Minesweeper/src/Field.java
@@ -8,6 +8,6 @@ public class Field {
 		Settings set = new Settings();
 		cellList = new Cell[set.getFieldWidth()][set.getFieldLenght()];
 		bombList = new Bomb[set.getBombAmount()];
-		
+		test
 	}
 }
