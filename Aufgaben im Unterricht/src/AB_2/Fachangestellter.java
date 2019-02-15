6c40931c78a175cde673c9c540b506a8d9fc8a65
bbated <38528348+bbated@users.noreply.github.com>
2019-02-15 09:13:38 +0100

    asdf

--- Aufgaben im Unterricht/src/AB_2/Fachangestellter.java
+++ Aufgaben im Unterricht/src/AB_2/Fachangestellter.java
@@ -4,8 +4,9 @@ class Fachangestellter extends Person {
 	
 	Chef vorgesetzter;
 	
-	public Fachangestellter(String Name, String Vorname, int PersonalNummer) {
+	public Fachangestellter(String Name, String Vorname, int PersonalNummer, Chef Vorgesetzter) {
 		super(Name, Vorname, PersonalNummer);
 		// TODO Auto-generated constructor stub
+		this.vorgesetzter = Vorgesetzter;
 	}
 }
