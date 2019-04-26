67113ef8fd36c3b4b0a917df0253aa11875b985c
bbated <38528348+bbated@users.noreply.github.com>
2019-04-26 08:47:09 +0200

    Update Rechteck.java
    ok

--- Aufgaben im Unterricht/src/Grafikeditor/Rechteck.java
+++ Aufgaben im Unterricht/src/Grafikeditor/Rechteck.java
@@ -1,5 +1,12 @@
 package Grafikeditor;
 
 public class Rechteck extends Figur {
-
+	private int breite;
+	private int höhe;
+	
+	public Rechteck (int Breite, int Höhe) {
+		
+		this.breite = Breite;
+		this.höhe = Höhe;
+	}
 }
