e92ea994ec1203f2f3febde7bf89da45f4dc13ce
bbated <38528348+bbated@users.noreply.github.com>
2019-05-03 08:21:40 +0200

    asdf

--- Aufgaben im Unterricht/src/Grafikeditor/main.java
+++ Aufgaben im Unterricht/src/Grafikeditor/main.java
@@ -1,9 +1,11 @@
 package Grafikeditor;
 
 public class main {
+	
+	private static final Display display = new Display();
 
 	public static void main(String[] args) {
-		private static final Display display = new Display();
+
 
 	}
 
